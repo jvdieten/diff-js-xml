12ba3c9812d21a70886631b893c7a8d8ee176fc4
Michael Bauer <mike.bauer@32past8.com>
2019-07-05 07:52:31 -0400

    Fixing JSON parse issue when XML declaration is present, added unit test for said fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import underscore from "underscore"
-import { xml2json } from "xml-js"
-import { IDiffResultModel } from "../lib/model/diff-result-model"
-import { IFieldOptions, ISchema } from "../lib/model/field-options-schema"
-import { IOptionsModel } from "../lib/model/options-model"
+import underscore from "underscore";
+import { xml2json } from "xml-js";
+import { IDiffResultModel } from "../lib/model/diff-result-model";
+import { IFieldOptions, ISchema } from "../lib/model/field-options-schema";
+import { IOptionsModel } from "../lib/model/options-model";
 
 const compareObjects = (
   a: Object,
@@ -11,51 +11,51 @@ const compareObjects = (
   keyPrefix: string | null,
   options: IOptionsModel
 ): IDiffResultModel[] => {
-  let differences: IDiffResultModel[] = []
-  const ak: string[] = Object.keys(a)
-  const bk: string[] = Object.keys(b)
-  const allKeys: string[] = underscore.union(ak, bk)
+  let differences: IDiffResultModel[] = [];
+  const ak: string[] = Object.keys(a);
+  const bk: string[] = Object.keys(b);
+  const allKeys: string[] = underscore.union(ak, bk);
 
   allKeys.forEach(key => {
-    const formattedKey: string = (keyPrefix || "") + key
-    const fieldOptions: IFieldOptions = schema[key] || {}
+    const formattedKey: string = (keyPrefix || "") + key;
+    const fieldOptions: IFieldOptions = schema[key] || {};
 
     if (!underscore.contains(ak, key)) {
       if (fieldOptions.skipKey) {
-        return
+        return;
       } else {
         const diffResult: IDiffResultModel = {
           path: formattedKey,
           resultType: "missing element",
           message: `field ${formattedKey} not present in lhs`
-        }
+        };
 
-        return differences.push(diffResult)
+        return differences.push(diffResult);
       }
     } else {
       //compare values
       if (fieldOptions.skipKey) {
-        return
+        return;
       } else {
         if (options.compareElementValues) {
-          let valueA
-          let valueB
+          let valueA;
+          let valueB;
           if ((<any>a)[key] !== undefined) {
-            valueA = (<any>a)[key].toString()
+            valueA = (<any>a)[key].toString();
           }
           if ((<any>b)[key] !== undefined) {
-            valueB = (<any>b)[key].toString()
+            valueB = (<any>b)[key].toString();
           }
           if (valueB !== undefined && valueA !== valueB && valueA !== "*") {
-            const resultValueA = JSON.stringify((<any>a)[key], null, 1)
-            const resultValueB = JSON.stringify((<any>b)[key], null, 1)
+            const resultValueA = JSON.stringify((<any>a)[key], null, 1);
+            const resultValueB = JSON.stringify((<any>b)[key], null, 1);
             const diffResult: IDiffResultModel = {
               path: formattedKey,
               resultType: "difference in element value",
               message: `field ${formattedKey} has lhs value ${resultValueA} and rhs value ${resultValueB}`
-            }
+            };
 
-            return differences.push(diffResult)
+            return differences.push(diffResult);
           }
         }
       }
@@ -63,49 +63,49 @@ const compareObjects = (
 
     if (!underscore.contains(bk, key)) {
       if (fieldOptions.skipKey) {
-        return
+        return;
       } else {
         const diffResult: IDiffResultModel = {
           path: formattedKey,
           resultType: "missing element",
           message: `field ${formattedKey} not present in rhs`
-        }
+        };
 
-        return differences.push(diffResult)
+        return differences.push(diffResult);
       }
     }
 
     if (underscore.isArray((<any>a)[key])) {
       for (let i = 0; i < (<any>a)[key].length; i++) {
-        const objA = (<any>a)[key][i]
-        const objB = (<any>b)[key][i]
+        const objA = (<any>a)[key][i];
+        const objB = (<any>b)[key][i];
 
         if (objA === "" && objB === "") {
-          return
+          return;
         }
         if (objA === "false" && objB === "false") {
-          return
+          return;
         }
         if (objA === 0 && objB === 0) {
-          return
+          return;
         }
         if (!objB) {
           const diffResult: IDiffResultModel = {
             path: formattedKey,
             resultType: "missing element",
             message: `field ${formattedKey}[${i}] not present in rhs`
-          }
+          };
 
-          return differences.push(diffResult)
+          return differences.push(diffResult);
         }
         if (!objA) {
           const diffResult: IDiffResultModel = {
             path: formattedKey,
             resultType: "missing element",
             message: `element ${formattedKey}[${i}] not present in lhs`
-          }
+          };
 
-          return differences.push(diffResult)
+          return differences.push(diffResult);
         }
         if (
           underscore.isObject((<any>a)[key][i]) &&
@@ -119,7 +119,7 @@ const compareObjects = (
               `${formattedKey}[${i}].`,
               options
             )
-          )
+          );
         }
       }
     } else if (underscore.isObject((<any>a)[key])) {
@@ -131,16 +131,16 @@ const compareObjects = (
           `${formattedKey}.`,
           options
         )
-      )
+      );
     }
-  })
+  });
 
-  return differences
-}
+  return differences;
+};
 
 // Remove _text key from  xml2json result to be able to reuse compare object function
 function adjustXMLforDiff(input: string): string {
-  return input.replace(/({"_(.*?)":)("(.*?)")(})/g, `$3`)
+  return input.replace(/({"_(.*?)":)("(.*?)")(})/g, `$3`);
 }
 
 export function diff(
@@ -158,7 +158,7 @@ export function diff(
       null,
       options || { compareElementValues: true }
     )
-  )
+  );
 }
 
 export function diffAsXml(
@@ -171,17 +171,19 @@ export function diffAsXml(
   const lhsp = xml2json(lhs, {
     compact: true,
     ignoreDoctype: true,
+    ignoreDeclaration: true,
     ignoreAttributes: true
-  })
+  });
   const rhsp = xml2json(rhs, {
     compact: true,
     ignoreDoctype: true,
+    ignoreDeclaration: true,
     ignoreAttributes: true
-  })
-  const lhsCompareString = adjustXMLforDiff(lhsp)
-  const rhsCompareString = adjustXMLforDiff(rhsp)
-  const jsonLhs = JSON.parse(lhsCompareString)
-  const jsonRhs = JSON.parse(rhsCompareString)
+  });
+  const lhsCompareString = adjustXMLforDiff(lhsp);
+  const rhsCompareString = adjustXMLforDiff(rhsp);
+  const jsonLhs = JSON.parse(lhsCompareString);
+  const jsonRhs = JSON.parse(rhsCompareString);
   next(
     compareObjects(
       jsonLhs,
@@ -190,5 +192,5 @@ export function diffAsXml(
       null,
       options || { compareElementValues: true }
     )
-  )
+  );
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,72 +1,72 @@
-import { should } from "chai"
-import * as tool from "../src/index"
+import { should } from "chai";
+import * as tool from "../src/index";
 
-import { IDiffResultModel } from "../lib/model/diff-result-model"
+import { IDiffResultModel } from "../lib/model/diff-result-model";
 
-should()
+should();
 
 describe("when comparing two identical objects", () => {
-  const lhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] }
-  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] }
+  const lhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] };
+  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] };
 
   it("should return an empty array of differences", () => {
     tool.diff(lhs, rhs, undefined, undefined, (result: IDiffResultModel[]) => {
-      result.length.should.equal(0)
-    })
-  })
-})
+      result.length.should.equal(0);
+    });
+  });
+});
 
 describe("when comparing two objects with a different value in a property", () => {
-  const lhs: Object = { a: { b: 11, c: "Hello" }, d: [1, 2, 3] }
-  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] }
+  const lhs: Object = { a: { b: 11, c: "Hello" }, d: [1, 2, 3] };
+  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] };
 
-  let result: IDiffResultModel[] = []
+  let result: IDiffResultModel[] = [];
 
   before(() => {
     tool.diff(lhs, rhs, undefined, undefined, (dff: IDiffResultModel[]) => {
-      result = dff
-    })
-  })
+      result = dff;
+    });
+  });
 
   it("should contain one different element", () => {
-    result.length.should.equal(1)
-  })
+    result.length.should.equal(1);
+  });
 
   it("should specify the different element", () => {
-    result[0].path.should.equal("a.b")
-  })
+    result[0].path.should.equal("a.b");
+  });
 
   it("should return the corrent message", () => {
-    result[0].resultType.should.equal("difference in element value")
-  })
-})
+    result[0].resultType.should.equal("difference in element value");
+  });
+});
 
 describe("when comparing two objects with differences in an array", () => {
-  const lhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 4] }
-  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] }
+  const lhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 4] };
+  const rhs: Object = { a: { b: 10, c: "Hello" }, d: [1, 2, 3] };
 
-  let result: IDiffResultModel[] = []
+  let result: IDiffResultModel[] = [];
 
   before(() => {
     tool.diff(lhs, rhs, undefined, undefined, (dff: IDiffResultModel[]) => {
-      result = dff
-    })
-  })
+      result = dff;
+    });
+  });
 
   it("should contain one different element", () => {
-    result.length.should.equal(1)
-  })
+    result.length.should.equal(1);
+  });
 
   it("should specify the correct element", () => {
-    result[0].path.should.equal("d")
-  })
-})
+    result[0].path.should.equal("d");
+  });
+});
 
 describe("when comparing two identical xml strings", () => {
   const lhsxml: string =
-    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>'
+    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
   const rhsxml: string =
-    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>'
+    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
 
   it("should return an empty array of differences", () => {
     tool.diffAsXml(
@@ -75,19 +75,19 @@ describe("when comparing two identical xml strings", () => {
       undefined,
       undefined,
       (result: IDiffResultModel[]) => {
-        result.length.should.equal(0)
+        result.length.should.equal(0);
       }
-    )
-  })
-})
+    );
+  });
+});
 
 describe("when schema with skip element is provided that is not in rhs xml", () => {
-  const schema = { "new-item": { skipKey: true } }
+  const schema = { "new-item": { skipKey: true } };
 
   const lhsxml: string =
-    '<string-array name="languages_array"><new-item>hallo</new-item><item>3</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><new-item>hallo</new-item><item>3</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
   const rhsxml: string =
-    '<string-array name="languages_array"><item>4</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><item>4</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
 
   it("should return an empty array of differences", () => {
     tool.diffAsXml(
@@ -96,19 +96,19 @@ describe("when schema with skip element is provided that is not in rhs xml", () 
       schema,
       undefined,
       (result: IDiffResultModel[]) => {
-        result.length.should.equal(1)
+        result.length.should.equal(1);
       }
-    )
-  })
-})
+    );
+  });
+});
 
 describe("when comparing two different xml element values with compareElementValues false", () => {
-  const optionsNoValueCompare = { compareElementValues: false }
+  const optionsNoValueCompare = { compareElementValues: false };
 
   const lhsxml: string =
-    '<string-array name="languages_array"><item>English2</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>'
+    '<string-array name="languages_array"><item>English2</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
   const rhsxml: string =
-    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>'
+    '<string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
 
   it("should return an empty array of differences", () => {
     tool.diffAsXml(
@@ -117,19 +117,19 @@ describe("when comparing two different xml element values with compareElementVal
       undefined,
       optionsNoValueCompare,
       (result: IDiffResultModel[]) => {
-        result.length.should.equal(0)
+        result.length.should.equal(0);
       }
-    )
-  })
-})
+    );
+  });
+});
 
 describe("when comparing two different xml values", () => {
   const lhsxml: string =
-    '<string-array name="languages_array"><item>3</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><item>3</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
   const rhsxml: string =
-    '<string-array name="languages_array"><item>4</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><item>4</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
 
-  let result: IDiffResultModel[] = []
+  let result: IDiffResultModel[] = [];
 
   before(() => {
     tool.diffAsXml(
@@ -138,27 +138,27 @@ describe("when comparing two different xml values", () => {
       undefined,
       undefined,
       (dff: IDiffResultModel[]) => {
-        result = dff
+        result = dff;
       }
-    )
-  })
+    );
+  });
 
   it("should contain one different element", () => {
-    result.length.should.equal(1)
-  })
+    result.length.should.equal(1);
+  });
 
   it("should specify the correct element", () => {
-    result[0].path.should.equal("string-array.item")
-  })
-})
+    result[0].path.should.equal("string-array.item");
+  });
+});
 
 describe("when comparing wildcard xml element values", () => {
   const lhsxml: string =
-    '<string-array name="languages_array"><item>*</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><item>*</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
   const rhsxml: string =
-    '<string-array name="languages_array"><item>Dutch</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>'
+    '<string-array name="languages_array"><item>Dutch</item><item2>Chinese</item2><item3>French</item3><item4>Spanish</item4></string-array>';
 
-  let result: IDiffResultModel[] = []
+  let result: IDiffResultModel[] = [];
 
   before(() => {
     tool.diffAsXml(
@@ -167,12 +167,31 @@ describe("when comparing wildcard xml element values", () => {
       undefined,
       undefined,
       (dff: IDiffResultModel[]) => {
-        result = dff
+        result = dff;
       }
-    )
-  })
+    );
+  });
 
   it("no differences are reported", () => {
-    result.length.should.equal(0)
-  })
-})
+    result.length.should.equal(0);
+  });
+});
+
+describe("when comparing two identical xml strings that have declarations", () => {
+  const lhsxml: string =
+    '<?xml version="1.0" encoding="UTF-8"?><string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
+  const rhsxml: string =
+    '<?xml version="1.0" encoding="UTF-8"?><string-array name="languages_array"><item>English</item><item>Chinese</item><item>French</item><item>Spanish</item></string-array>';
+
+  it("should return an empty array of differences without failing to parse the resulting JSON", () => {
+    tool.diffAsXml(
+      lhsxml,
+      rhsxml,
+      undefined,
+      undefined,
+      (result: IDiffResultModel[]) => {
+        result.length.should.equal(0);
+      }
+    );
+  });
+});
